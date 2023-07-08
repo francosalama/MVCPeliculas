abad0d0f0ed5b0fe6ab849686b9851658e96a043
francosalama <salamafranco@gmail.com>
2023-07-07 21:32:28 -0300

    estilo y validaciones

--- MVCPeliculas/Controllers/PeliculaVistaController.cs
+++ MVCPeliculas/Controllers/PeliculaVistaController.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
--- MVCPeliculas/Models/Pelicula.cs
+++ MVCPeliculas/Models/Pelicula.cs
@@ -10,16 +10,30 @@ namespace MVCPeliculas.Models
 		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 		public int Id { get; set; }
 
+        [Required(ErrorMessage = "Falta llenar campo de nombre")]
         public string Nombre { get; set; }
+
         [Display(Name = "Imagen")]
+        [Required(ErrorMessage = "Falta llenar campo de imagen")]
         public string LinkFoto { get; set; }
+
+        [Range(1, 5, ErrorMessage = "Solo se admiten una valoracion entre {1} y {2}")]
+        [Required(ErrorMessage = "Falta llenar campo de valoracion")]
         public int Valoracion { get; set; }
+
         [Display(Name = "Año")]
+        [Required(ErrorMessage = "Falta llenar campo de año")]
         public int Anio { get; set; }
+
+        [Required(ErrorMessage = "Falta llenar campo de sinopsis")]
         public string Sinopsis { get; set; }
-		[EnumDataType(typeof(Genero))]
+
+        [Required(ErrorMessage = "Falta llenar campo de genero")]
+        [EnumDataType(typeof(Genero))]
 		public Genero Genero { get; set; }
-		[EnumDataType(typeof(Plataforma))]
+
+        [Required(ErrorMessage = "Falta llenar campo de plataforma")]
+        [EnumDataType(typeof(Plataforma))]
 		public Plataforma Plataforma { get; set; }
         public List<PeliculaVista> UsuariosVistos { get; set; }
         public List<PeliculaDeseada> UsuariosDeseados { get; set; }
